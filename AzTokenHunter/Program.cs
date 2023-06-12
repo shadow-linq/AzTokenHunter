8e389ce4d03435cf9d08d8d333a1abd63ece7135
shadow-linq <133143219+shadow-linq@users.noreply.github.com>
2023-06-12 13:40:35 -0600

    Update Program.cs
    Improved regex search pattern for the tool

--- AzTokenHunter/Program.cs
+++ AzTokenHunter/Program.cs
@@ -98,7 +98,7 @@ namespace AzTokenHunter
         
         public static void FindValidJWTs(string input)
         {
-            string pattern = @"\beyJ0[A-Za-z0-9-_\.]+[=]?\b"; //To-do: Improve regex search pattern
+            string pattern = @"\beyJ0[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+[=]?\b";
             string systemRoot = Environment.GetEnvironmentVariable("SystemRoot");
             Regex regex = new Regex(pattern);
             MatchCollection matches = regex.Matches(input);
